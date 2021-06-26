8a1169d280b69c63c5027d8bf6653dc2e493f6b2
Matthew Fitzpatrick <50877393+Mattfitzpatrick97@users.noreply.github.com>
2021-06-25 20:21:41 -0400

    Add files via upload

---
+++ Matthew-991524179-Midterm/Matthew-991524179-Midterm/Functionalities.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Matthew_991524179_Midterm
+{
+	/// <summary>
+	/// Interaction logic for Functionalities.xaml
+	/// </summary>
+	public partial class Functionalities : Page
+	{
+		public Functionalities()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Matthew-991524179-Midterm/Matthew-991524179-Midterm/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Matthew_991524179_Midterm
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		private void Functionalities_Click(object sender, RoutedEventArgs e)
+		{
+			Functionalities functions = new Functionalities();
+			Functionalities.Show();
+			this.Close();
+		}
+
+		private void Quit_Click(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+	}
+}
